c89b97329a743a339fda24cc94aa809b43bfa0d2
karanwadhwa <wadhwa.karan96@gmail.com>
2019-01-16 00:20:46 +0530

    remove header from AuthStackNavigator

--- src/navigation/AuthStackNavigator.js
+++ src/navigation/AuthStackNavigator.js
@@ -1,13 +1,23 @@
+import React from "react";
+import { View } from "react-native";
 import { createStackNavigator } from "react-navigation";
+import Icon from "@expo/vector-icons/Ionicons";
 
 import WelcomeScreen from "../screens/WelcomeScreen";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 
-const AuthStackNavigator = createStackNavigator({
-  Welcome: WelcomeScreen,
-  Login: LoginScreen,
-  Register: RegisterScreen
-});
+const AuthStackNavigator = createStackNavigator(
+  {
+    Welcome: WelcomeScreen,
+    Login: LoginScreen,
+    Register: RegisterScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      header: null
+    })
+  }
+);
 
 export default AuthStackNavigator;
