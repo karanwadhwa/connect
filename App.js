79ac26bbcdf16d112df602431d2ee7fe0ec7c6a6
karanwadhwa <wadhwa.karan96@gmail.com>
2019-01-15 15:33:06 +0530

    refactored App.js

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React from "react";
-
+import React, { Component } from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
 import AuthLoadingScreen from "./src/screens/AuthLoadingScreen";
 import AuthStackNavigator from "./src/navigation/AuthStackNavigator";
 import AppDrawerNavigator from "./src/navigation/AppDrawerNavigator";
+
+class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
 
 const RootSwitchNavigator = createSwitchNavigator({
   AuthLoadingScreen: AuthLoadingScreen,
@@ -12,4 +17,6 @@ const RootSwitchNavigator = createSwitchNavigator({
   App: AppDrawerNavigator
 });
 
-export default createAppContainer(RootSwitchNavigator);
+const AppContainer = createAppContainer(RootSwitchNavigator);
+
+export default App;
