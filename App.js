7ca02859c56c9fdc407270d1f0dceebee3c30e67
karanwadhwa <wadhwa.karan96@gmail.com>
2019-01-15 16:31:57 +0530

    basic redux store setup

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { Provider } from "react-redux";
 
 import AuthLoadingScreen from "./src/screens/AuthLoadingScreen";
 import AuthStackNavigator from "./src/navigation/AuthStackNavigator";
 import AppDrawerNavigator from "./src/navigation/AppDrawerNavigator";
 
+import store from "./src/store";
+
 class App extends Component {
   render() {
-    return <AppContainer />;
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
   }
 }
 
