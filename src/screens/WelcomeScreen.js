10575a8547c1cbb30d03df92683349bee69a6918
karanwadhwa <wadhwa.karan96@gmail.com>
2019-01-19 16:49:54 +0530

    WelcomeScreen design

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  StatusBar,
+  Platform
+} from "react-native";
+
+const { width } = Dimensions.get("window");
 
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Button
-          title="Login"
-          onPress={() => this.props.navigation.navigate("Login")}
-        />
-        <Button
-          title="Register"
-          onPress={() => this.props.navigation.navigate("Register")}
-        />
+      <View style={[styles.container, styles.androidHeader]}>
+        <View style={styles.header} />
+        <View style={styles.topSection}>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Login")}
+          >
+            <Text style={{ fontSize: 30, opacity: 0.6 }}>Login</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.polyClip} />
+        <View style={styles.bottomSection}>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Register")}
+          >
+            <Text style={{ fontSize: 30, opacity: 0.6 }}>Register</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.footer} />
       </View>
     );
   }
@@ -23,7 +42,55 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    //backgroundColor: "#E4E4E4",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  androidHeader: {
+    ...Platform.select({
+      android: {
+        paddingTop: StatusBar.currentHeight
+      }
+    })
+  },
+
+  header: {
+    height: 100,
+    width: "100%",
+    backgroundColor: "#E4E4E4"
+  },
+  footer: {
+    height: 100,
+    width: "100%",
+    backgroundColor: "#F2F2F2"
+  },
+
+  polyClip: {
+    backgroundColor: "transparent",
+    borderStyle: "solid",
+    // shape width
+    borderRightWidth: width,
+    // shape height
+    borderTopWidth: 150,
+    borderRightColor: "#F2F2F2",
+    borderTopColor: "#E4E4E4"
+  },
+
+  topSection: {
+    backgroundColor: "#E4E4E4",
+    width: "100%",
+    flex: 1,
+    //height: height / 2 - 50,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  bottomSection: {
+    backgroundColor: "#F2F2F2",
+    width: "100%",
+    flex: 1,
+    //height: height / 2 - 50,
     alignItems: "center",
     justifyContent: "center"
   }
