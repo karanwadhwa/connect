aceb89beba09e580ba941c9c7fdb06ef20415747
karanwadhwa <wadhwa.karan96@gmail.com>
2019-01-19 18:24:11 +0530

    used expo-AppLoading to load resources before rendering the AppContainer

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { Provider } from "react-redux";
-import { Font } from "expo";
+import { AppLoading, Font, Asset } from "expo";
 
 import AuthLoadingScreen from "./src/screens/AuthLoadingScreen";
 import AuthStackNavigator from "./src/navigation/AuthStackNavigator";
@@ -10,21 +10,49 @@ import AppDrawerNavigator from "./src/navigation/AppDrawerNavigator";
 import store from "./src/store";
 
 class App extends Component {
-  componentDidMount() {
-    Font.loadAsync({
-      "nunito-regular": require("./src/assets/fonts/Nunito-Regular.ttf"),
-      "nunito-light": require("./src/assets/fonts/Nunito-Light.ttf"),
-      "nunito-bold": require("./src/assets/fonts/Nunito-Bold.ttf"),
-      "nunito-extra-bold": require("./src/assets/fonts/Nunito-ExtraBold.ttf")
-    });
-  }
+  state = {
+    isLoadingComplete: false
+  };
+
   render() {
-    return (
-      <Provider store={store}>
-        <AppContainer />
-      </Provider>
-    );
+    if (!this.state.isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <Provider store={store}>
+          <AppContainer />
+        </Provider>
+      );
+    }
   }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Font.loadAsync({
+        "nunito-regular": require("./src/assets/fonts/Nunito-Regular.ttf"),
+        "nunito-light": require("./src/assets/fonts/Nunito-Light.ttf"),
+        "nunito-bold": require("./src/assets/fonts/Nunito-Bold.ttf"),
+        "nunito-extra-bold": require("./src/assets/fonts/Nunito-ExtraBold.ttf")
+      })
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+    // Will use this later to show a 404 splash page
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
 }
 
 const RootSwitchNavigator = createSwitchNavigator({
