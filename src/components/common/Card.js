2dd04f108d55b9750948f60d199c27c32c592baa
karanwadhwa <wadhwa.karan96@gmail.com>
2019-01-26 03:10:57 +0530

    common Card component

---
+++ src/components/common/Card.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, StyleSheet, Platform } from "react-native";
+
+const Card = props => {
+  return <View style={styles.cardContainer}>{props.children}</View>;
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: "#ddd",
+    ...Platform.select({
+      ios: {
+        shadowColor: "#000",
+        shadowOffset: { height: 2, width: 0 },
+        shadowOpacity: 0.1,
+        shadowRadius: "2"
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
+});
