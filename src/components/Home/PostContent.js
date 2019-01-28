a8cb9b6b9a71a237a8a9f16942a85087bcf512cf
karanwadhwa <wadhwa.karan96@gmail.com>
2019-01-28 18:00:28 +0530

    convert PostContent to a class-based component

--- src/components/Home/PostContent.js
+++ src/components/Home/PostContent.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet } from "react-native";
 import { View, Row, Caption, Text } from "@shoutem/ui";
 
-const PostContent = ({ post }) => {
-  return (
-    <Row>
-      <View>
-        <Text styleName="multiline">{post.body}</Text>
-        <View styleName="horizontal" style={{ paddingTop: 5 }}>
-          <Caption styleName="bold" style={styles.meta}>
-            {post.likes.length} {post.likes.length === 1 ? "Like" : "Likes"}
-            {"\t \t"}
-          </Caption>
-          <Caption styleName="bold" style={styles.meta}>
-            {post.comments.length}{" "}
-            {post.comments.length === 1 ? "Comment" : "Comments"}
-          </Caption>
+class PostContent extends Component {
+  render() {
+    const { body, likes, comments } = this.props.post;
+    return (
+      <Row>
+        <View>
+          <Text styleName="multiline">{body}</Text>
+          <View styleName="horizontal" style={{ paddingTop: 5 }}>
+            <Caption styleName="bold" style={styles.meta}>
+              {likes.length} {likes.length === 1 ? "Like" : "Likes"}
+              {"\t \t"}
+            </Caption>
+            <Caption styleName="bold" style={styles.meta}>
+              {comments.length} {comments.length === 1 ? "Comment" : "Comments"}
+            </Caption>
+          </View>
         </View>
-      </View>
-    </Row>
-  );
-};
+      </Row>
+    );
+  }
+}
 
 export default PostContent;
 
