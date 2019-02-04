f160beceb830e11db5eeb61a5aba98a516cc27ca
karanwadhwa <wadhwa.karan96@gmail.com>
2019-02-05 05:26:24 +0530

    CreatePostPage form redesign with Audience ActionSheet

--- src/components/NewPost/CreatePostPage.js
+++ src/components/NewPost/CreatePostPage.js
@@ -1,25 +1,75 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import {
-  Text,
   View,
+  Text,
   ScrollView,
-  KeyboardAvoidingView,
   TouchableOpacity,
   Alert,
   StyleSheet,
   Platform,
-  StatusBar
+  StatusBar,
+  TextInput
 } from "react-native";
 import { WebBrowser } from "expo";
-import { Formik } from "formik";
-import * as Yup from "yup";
+import { ActionSheetCustom as ActionSheet } from "react-native-actionsheet";
 
-import FormInput from "../common/FormInput";
 import HorizontalRule from "../common/HorizontalRule";
 
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 class CreatePostPage extends Component {
-  _handleSubmit = (values, bag) => {
-    Alert.alert(values.title, values.body);
+  constructor() {
+    super();
+
+    this.state = {
+      title: "",
+      postAudience: [],
+      body: "",
+      errors: {
+        title: "",
+        audience: "",
+        body: ""
+      }
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.user.audience.length === 0) {
+      this.setState({
+        errors: { audience: "No audience has been assigned to you yet." }
+      });
+    }
+  }
+
+  handleSubmit = () => {
+    Alert.alert(
+      `Title: ${this.state.title}`,
+      `Audience: ${this.state.audience}\nPost: ${this.state.body}`
+    );
+    console.log(this.state);
+  };
+
+  renderSelectedAudience = () => {};
+
+  renderActionSheet = () => {
+    return (
+      <ActionSheet
+        ref={o => (this.ActionSheet = o)}
+        title="Select your audience"
+        message="Only the audience you select will receive this post."
+        options={this.props.user.audience.concat(["Cancel"])}
+        cancelButtonIndex={this.props.user.audience.length}
+        destructiveButtonIndex={this.props.user.audience.length}
+        onPress={index => {
+          console.log(index);
+        }}
+      />
+    );
+  };
+
+  showActionSheet = () => {
+    this.ActionSheet.show();
   };
 
   render() {
@@ -29,87 +79,86 @@ class CreatePostPage extends Component {
           <Text style={styles.title}>Create Post</Text>
         </View>
         <View style={styles.formContainer}>
-          <KeyboardAvoidingView
-          //enableAutomaticScroll={true}
-          //enableOnAndroid={true}
-          >
-            <Formik
-              initialValues={{ audience: [], body: "", title: "" }}
-              onSubmit={this._handleSubmit}
-              validationSchema={Yup.object().shape({
-                body: Yup.string().required()
-              })}
-              render={({
-                values,
-                handleSubmit,
-                setFieldValue,
-                errors,
-                touched,
-                setFieldTouched,
-                isValid,
-                setSubmitting
-              }) => (
-                <React.Fragment>
-                  <FormInput
-                    label="Title"
-                    placeholder="Post Title"
-                    returnKeyType="next"
-                    blurOnSubmit={false}
-                    name="title"
-                    value={values.title}
-                    onChange={setFieldValue}
-                    onTouch={setFieldTouched}
-                    error={errors.title}
-                  />
-
-                  <FormInput
-                    label="Post Body"
-                    multiline={true}
-                    height={200}
-                    textAlignVertical="top"
-                    returnKeyType="go"
-                    name="body"
-                    value={values.body}
-                    onChange={setFieldValue}
-                    onTouch={setFieldTouched}
-                    error={touched.post && errors.post}
-                  />
-
-                  <View style={styles.buttonContainer}>
-                    <TouchableOpacity
-                      style={styles.button}
-                      onPress={handleSubmit}
-                      disabled={!isValid}
-                    >
-                      <Text style={styles.buttonText}>SUBMIT</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      style={{ width: "100%", alignItems: "center" }}
-                      onPress={() =>
-                        WebBrowser.openBrowserAsync(
-                          "https://www.markdownguide.org/basic-syntax"
-                        )
-                      }
-                    >
-                      <Text
-                        style={{ color: "#107AFB", fontFamily: "nunito-bold" }}
-                      >
-                        All posts are markdown compatible. Read more.
-                      </Text>
-                    </TouchableOpacity>
-                  </View>
-                  <HorizontalRule width="80%" />
-                </React.Fragment>
-              )}
+          <Text style={styles.label}>Title</Text>
+          <Text style={styles.error}>{this.state.errors.title}</Text>
+          <View style={styles.inputBox}>
+            <TextInput
+              value={this.state.title}
+              onChangeText={title => this.setState({ title })}
+              style={{ width: "100%" }}
+              placeholder="Post Title (optional)"
+              placeholderTextColor="rgba(172,172,172,0.5)"
             />
-          </KeyboardAvoidingView>
+          </View>
+
+          <View
+            style={{
+              backgroundColor: "",
+              justifyContent: "space-between",
+              flexDirection: "row"
+            }}
+          >
+            <Text style={styles.label}>Audience</Text>
+            <TouchableOpacity onPress={this.showActionSheet}>
+              <Ionicons name="ios-add-circle-outline" size={18} />
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.error}>{this.state.errors.audience}</Text>
+          <View style={{ marginBottom: 20 }} />
+          {this.renderActionSheet()}
+
+          <Text style={styles.label}>Post Body</Text>
+          <Text style={styles.error}>{this.state.errors.body}</Text>
+          <View style={[styles.inputBox, { height: 200 }]}>
+            <TextInput
+              value={this.state.body}
+              onChangeText={body => this.setState({ body })}
+              textAlignVertical="top"
+              multiline={true}
+              returnKeyType="go"
+              style={{ width: "100%", height: "100%" }}
+              placeholderTextColor="rgba(172,172,172,0.5)"
+            />
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={this.handleSubmit}>
+              <Text style={styles.buttonText}>SUBMIT</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ width: "100%", alignItems: "center" }}
+              onPress={() =>
+                WebBrowser.openBrowserAsync(
+                  "https://www.markdownguide.org/basic-syntax"
+                )
+              }
+            >
+              <Text
+                style={{
+                  color: "#107AFB",
+                  fontFamily: "nunito-bold",
+                  textAlign: "center"
+                }}
+              >
+                All posts are markdown compatible. Read more.
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <HorizontalRule width="80%" />
         </View>
       </ScrollView>
     );
   }
 }
 
-export default CreatePostPage;
+const mapStateToProps = state => {
+  return {
+    accessToken: state.auth.accessToken,
+    user: state.auth.user
+  };
+};
+
+export default connect(mapStateToProps)(CreatePostPage);
 
 const styles = StyleSheet.create({
   container: {
@@ -136,20 +185,25 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 40
   },
-  inputLabel: {
-    fontWeight: "700",
-    //fontFamily: "nunito-bold",
-    color: "#333333",
-    opacity: 0.8,
-    paddingBottom: 7
+  label: {
+    fontFamily: "Rubik-Regular",
+    color: "#333333"
   },
-  input: {
+  error: {
+    fontWeight: "500",
+    color: "red",
+    opacity: 0.7
+  },
+  inputBox: {
     height: 45,
     borderWidth: 1,
     borderColor: "rgba(172,172,172,0.7)",
     marginBottom: 20,
     paddingHorizontal: 10,
-    borderRadius: 5
+    borderRadius: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   buttonContainer: {
     marginTop: 10,
