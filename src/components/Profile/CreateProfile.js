d077b3f49f789f1bb9babb611227944d78f73ec5
karanwadhwa <wadhwa.karan96@gmail.com>
2019-02-19 06:28:18 +0530

    CreateProfile design changes for non student/professor users

--- src/components/Profile/CreateProfile.js
+++ src/components/Profile/CreateProfile.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
-import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+  StyleSheet,
+  AsyncStorage
+} from "react-native";
 import { connect } from "react-redux";
+import { Subtitle, Button, Heading } from "@shoutem/ui";
 
 import CreateNewStudentProfile from "./CreateNewStudentProfile";
 import CreateNewProfessorProfile from "./CreateNewProfessorProfile";
 
 class CreateProfile extends Component {
+  logout = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate("AuthLoadingScreen");
+  };
+
   render() {
     if (this.props.authLoading) {
       return (
@@ -19,8 +32,29 @@ class CreateProfile extends Component {
       }
 
       return (
-        <View>
-          <Text>User Type is neither student nor professor</Text>
+        <View style={styles.container}>
+          <Heading>Profile not found!</Heading>
+          <View
+            style={{
+              paddingVertical: 20
+            }}
+          >
+            <Subtitle styleName="h-center">
+              You cannot proceed without setting up your profile.
+            </Subtitle>
+            <Subtitle styleName="h-center">
+              <Subtitle
+                style={{ color: "#107AFB", fontFamily: "nunito-extra-bold" }}
+              >
+                {this.props.user.userType}{" "}
+              </Subtitle>
+              users should have their profiles setup manually. Contact the
+              systems administrator for more help.
+            </Subtitle>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={this.logout}>
+            <Text style={styles.buttonText}>LOGOUT</Text>
+          </TouchableOpacity>
         </View>
       );
     }
@@ -41,5 +75,23 @@ const styles = StyleSheet.create({
     flex: 1,
     alignSelf: "center",
     justifyContent: "center"
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30
+  },
+  button: {
+    backgroundColor: "#107AFB",
+    marginBottom: 10,
+    paddingVertical: 14,
+    borderRadius: 5,
+    width: "100%"
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontFamily: "nunito-extra-bold"
   }
 });
